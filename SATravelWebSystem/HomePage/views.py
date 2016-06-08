8b1768855f94b4a6b1482f300fb5782d3f3f8e54
ntude <goxluix7@yahoo.com.tw>
2016-06-08 22:33:55 +0800

    add comming soon fixed listPage z-index add chouchou calendar

--- SATravelWebSystem/HomePage/views.py
+++ SATravelWebSystem/HomePage/views.py
@@ -9,8 +9,8 @@ def home(request) :
     #     account = request.POST['account']
     #     password = request
     region1 = {'region': '台北', 'school': '台大', 'name': '台  大', 'image': 'static/images/taiwan_university.png'}
-    region2 = {'region': '新竹', 'school': '清大', 'name': '清  大', 'image': 'static/images/chin-hua.png'}
-    region3 = {'region': '新竹', 'school': '交大', 'name': '交  大', 'image': 'static/images/mrt.png'}
+    region2 = {'region': '新竹', 'school': '清大', 'name': '清  大<br><b>Comming Soon...</b>', 'image': 'static/images/chin-hua.png'}
+    region3 = {'region': '新竹', 'school': '交大', 'name': '交  大<br><b>Comming Soon...</b>', 'image': 'static/images/mrt.png'}
     top_regions = [region1, region2, region3]
     images = ['static/images/ranbow.png', 'static/images/paper.png',
             'static/images/sasasa.png', 'static/images/beauty.png',
